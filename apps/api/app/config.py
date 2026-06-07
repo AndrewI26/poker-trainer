cf523d7a4968129648ed149d340c10e82295a31f
AndrewI26 <andrewi44@icloud.com>
2026-06-06 22:07:12 -0400

    Moved .env stuff to root dir

--- apps/api/app/config.py
+++ apps/api/app/config.py
@@ -2,10 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    model_config = SettingsConfigDict(env_file=("../../.env", ".env"), env_file_encoding="utf-8", extra="ignore")
 
-    database_url: str = "postgresql+psycopg://postgres:postgres@localhost:5432/poker_trainer"
+    database_url: str
+    api_port: int
     api_title: str = "Poker Trainer API"
 
 
-settings = Settings()
+settings = Settings() # type: ignore
